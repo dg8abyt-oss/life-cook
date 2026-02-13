565aaba583210a5b6b6617506a2a0f15098531f1
dg8abyt-oss <dg8abyt@gmail.com>
2026-02-13 08:01:48 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,18 @@
 /**
  * LifeCook Service Worker
- * Handles cross-device global notifications
  */
 
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
+const ICON = "https://ik.imagekit.io/migbb/image.jpeg?updatedAt=1770995065553";
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
 
-// This is triggered by the reg.showNotification in the main script
 self.addEventListener('notificationclick', (event) => {
     event.notification.close();
     event.waitUntil(
         clients.matchAll({ type: 'window' }).then(windowClients => {
-            if (windowClients.length > 0) {
-                windowClients[0].focus();
-            } else {
-                clients.openWindow('/');
-            }
+            if (windowClients.length > 0) return windowClients[0].focus();
+            return clients.openWindow('/');
         })
     );
 });
